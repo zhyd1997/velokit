6d2b5b8117acd632b487980837b50ecd365e9ef8
Yadong Zhang <zhyd007@gmail.com>
2025-06-07 21:40:31 +0800

    Integrate Prisma client and database schema into web application; update package dependencies and configurations for database access. Add new db:generate task in Turbo configuration and adjust TypeScript paths for improved type safety.

---
+++ apps/web/types/model.ts
@@ -0,0 +1,49 @@
+// import { Prisma } from "@workspace/db";
+
+// export const DB_SCHEMA = "public";
+
+// type BaseFields =
+//   | "id"
+//   | "created_at"
+//   | "updated_at"
+//   | "created_by"
+//   | "updated_by";
+
+// type NullableFields<T> = {
+//   [K in keyof T]: null extends T[K] ? K : never;
+// }[keyof T];
+
+// type RequiredFields<T> = {
+//   [K in keyof T]: null extends T[K] ? never : K;
+// }[keyof T];
+
+// type CreateTableType<T> = {
+//   Row: T;
+//   Insert: {
+//     [K in RequiredFields<Omit<T, BaseFields>>]: Omit<T, BaseFields>[K];
+//   } & {
+//     [K in NullableFields<Omit<T, BaseFields>>]?: Omit<T, BaseFields>[K];
+//   };
+//   Update: Partial<T>;
+// };
+
+// export type Database = {
+//   [DB_SCHEMA]: {
+//     Tables: {
+//       users: CreateTableType<Prisma.UserGetPayload<{}>>;
+//     };
+//   };
+// };
+
+// /**
+//  * type X = Table<"users">
+//  *
+//  * @example
+//  * const x: X["Row"]
+//  */
+// export type Table<T extends keyof Database[typeof DB_SCHEMA]["Tables"]> =
+//   Database[typeof DB_SCHEMA]["Tables"][T];
+
+// export type UsersType = Table<"users">;
+
+export type Database = any;
