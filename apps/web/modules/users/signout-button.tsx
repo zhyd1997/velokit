07c4f39258930cb262070065e856881c3482e71b
Yadong Zhang <zhyd007@gmail.com>
2025-06-08 12:07:22 +0800

    feat(apps/web): replace logout button with `SignoutButton` component for improved user experience

---
+++ apps/web/modules/users/signout-button.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import type { FC } from "react";
+
+import { useTransition } from "react";
+
+import { Button } from "@workspace/ui/components/button";
+import { logOut } from "@/app/actions/auth";
+
+export type SignoutButtonProps = {};
+
+export const SignoutButton: FC<SignoutButtonProps> = () => {
+  const [isPending, startTransition] = useTransition();
+
+  const handleLogout = () => {
+    startTransition(async () => logOut());
+  };
+
+  return (
+    <Button className="hover:cursor-pointer" onClick={handleLogout}>
+      {isPending ? "Signing out..." : "Sign Out"}
+    </Button>
+  );
+};
